edacd51d24736d35690dc91025d8e24b458501b3
João Francisco <targino.joao@gmail.com>
2025-09-16 15:50:55 -0300

    Atualizar 'main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -1,3 +1,21 @@
+input.onButtonPressed(Button.A, function () {
+    Sabrina.move(-1)
+})
+input.onButtonPressed(Button.B, function () {
+    Sabrina.move(1)
+})
+let Sabrina: game.LedSprite = null
+Sabrina = game.createSprite(2, 4)
+let Pinha = game.createSprite(randint(0, 4), 0)
 basic.forever(function () {
-	
+    basic.pause(500)
+    Pinha.change(LedSpriteProperty.Y, 1)
+    if (Sabrina.isTouching(Pinha)) {
+        game.gameOver()
+    }
+    if (Pinha.get(LedSpriteProperty.Y) == 4) {
+        basic.pause(100)
+        Pinha.set(LedSpriteProperty.Y, 0)
+        Pinha.set(LedSpriteProperty.X, randint(0, 4))
+    }
 })
